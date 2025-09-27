3fc0082050a360fff883f7203113a30e8fb94324
Kylee Tilley <kyleetilley@gmail.com>
2025-09-27 00:46:52 -0500

    Add expected ast to spec tests

--- tests/spec_tests.rs
+++ tests/spec_tests.rs
@@ -1,5 +1,5 @@
-use language_project_template::errors::ExprResult;
 use language_project_template::lexer::lex;
+use language_project_template::{errors::ExprResult, parser::parse};
 use rstest::rstest;
 use std::{fs::read_to_string, path::PathBuf};
 
@@ -11,11 +11,23 @@ fn spec_files_tokens(#[files("spec/**/*.expr")] path: PathBuf) -> ExprResult<()>
         let expected_tokens =
             read_to_string(expected_tokens_path).expect("should be able to read file");
 
-        let expr_source = read_to_string(path).expect("should be able to read file");
+        let expr_source = read_to_string(&path).expect("should be able to read file");
 
         let tokens = lex(&expr_source);
 
         pretty_assertions::assert_eq!(format!("{tokens:#?}"), expected_tokens);
+    }
+
+    let expected_ast_path = path.with_extension("expr.ast");
+
+    if expected_ast_path.exists() {
+        let expected_ast = read_to_string(expected_ast_path).expect("should be able to read file");
+
+        let expr_source = read_to_string(&path).expect("should be able to read file");
+
+        let ast = parse(&expr_source);
+
+        pretty_assertions::assert_eq!(format!("{ast:#?}"), expected_ast);
     }
 
     Ok(())
